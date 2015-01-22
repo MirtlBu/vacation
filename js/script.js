0f2790242a2fbee690a60c4eefeffdc40348f67c
mirtlbu <bubuzoglo@gmail.com>
2015-01-22 14:35:49 +0300

    in the middle of the gallery

--- js/script.js
+++ js/script.js
@@ -1,10 +1,52 @@
-
-
 $(function() {
+
     $('.schedule').on('mouseenter', '.schedule__item', function() {
        $(this).addClass('schedule__item--shifted');
     });
     $('.schedule').on('mouseleave', '.schedule__item', function() {
        $(this).removeClass('schedule__item--shifted');
+    });
+
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > $('.opening').height()){
+            $('header').addClass('sticky');
+            $('.schedule').addClass('debounce');
+        }
+        else {
+            $('.schedule').removeClass('debounce');
+            $('header').removeClass('sticky');
+        }
+    });
+
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+    $(window).load(function() {
+      $('.gallery__slider').flexslider({
+        animation: "slide",
+        controlNav: "thumbnails",
+        selector: 'ul > li',
+        slideshow: false
+      });
+    });
+
+    $('.gallery').on('click', '.gallery__item', function() {
+        $('.modal-overlay').addClass('showed');
+        $('body').addClass('scrolldisable');
     })
+
 });
+
+
